bb3e5cbf78e5299c9ce90fc5d74cdc0914877de0
ppidcard <tvxqlikei@live.cn>
2021-07-27 13:39:22 +1000

    Done

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,39 @@
 class Customer
-  
+    attr_accessor :name, :age
+    @@all = []
+
+    def initialize(name, age)
+        @name = name
+        @age = age
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(waiter, total, tip = 0)
+        Meal.new(waiter, self, total, tip)
+    end
+
+    def meals
+        Meal.all.select{|meal| meal.customer == self}
+    end
+
+    def waiters
+        meals.map{|meal| meal.waiter}
+    end
+
 end
+
+alex = Customer.new("Alex", 30)
+rachel = Customer.new("Rachel", 27)
+dan = Waiter.new("Dan", 3)
+
+rachel.new_meal(dan, 50, 10)
+alex.new_meal(dan, 30, 5)
+
+rachel.meals #=> [#, @customer=#, @total=50, @tip=10>]
+rachel.meals.length #=> 1
+
+Meal.all.length #=> 2
